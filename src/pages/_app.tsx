2230010f669ab796c875d6c6599ac9c292a9dfbb
Ravi Garbuja Pun <cbpgmr@gmail.com>
2025-07-01 21:00:27 +1000

    style: Add type annotations for Component and pageProps
    Add type annotations for the `Component` and `pageProps` props using Next.js's `AppProps` type from `next/app`.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
+import type { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+export default MyApp; 
